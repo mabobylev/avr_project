f77c02a5658d651e016ef5f569f183056aeee208
Mihail Bobylev <mabobylev@gmail.com>
2024-06-26 21:46:31 +0400

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "main.h"
+
+int main(void) {
+    while (1) {
+        _delay_ms(1000);
+    }
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#define __AVR_ATmega8A__
+#define F_CPU 8000000UL
+
+#endif
